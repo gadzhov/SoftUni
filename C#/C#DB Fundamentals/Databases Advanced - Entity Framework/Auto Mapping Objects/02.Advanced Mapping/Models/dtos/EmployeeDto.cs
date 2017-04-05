82ed497af6f2078c9790cfc10671ca169733eeea
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2017-04-05 17:31:15 +0300

    Add files via upload

---
+++ C#/C#DB Fundamentals/Databases Advanced - Entity Framework/Auto Mapping Objects/02.Advanced Mapping/Models/dtos/EmployeeDto.cs
@@ -0,0 +1,9 @@
+﻿namespace _02.Advanced_Mapping.Models
+{
+    public class EmployeeDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public decimal Salary { get; set; }
+    }
+}
