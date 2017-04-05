73d630d2fa22f97277a49ec5ae37f2f43d56313d
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2017-04-05 18:11:39 +0300

    Add files via upload

---
+++ C#/C#DB Fundamentals/Databases Advanced - Entity Framework/XML Processing/ProductsShop/Data/Init.cs
@@ -0,0 +1,13 @@
+﻿namespace Data
+{
+    public class Init
+    {
+        public static void Run()
+        {
+            using (var context = new ProductShopContext())
+            {
+                context.Database.Initialize(true);
+            }
+        }
+    }
+}
