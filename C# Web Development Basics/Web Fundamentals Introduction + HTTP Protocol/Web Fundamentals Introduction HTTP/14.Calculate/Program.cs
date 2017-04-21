5697e2a8d01c2b095ad7e2a2440e67d545f122dd
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2017-04-22 00:12:50 +0300

    Add files via upload

---
+++ C# Web Development Basics/Web Fundamentals Introduction + HTTP Protocol/Web Fundamentals Introduction HTTP/14.Calculate/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+
+namespace _14.Calculate
+{
+    class Program
+    {
+         static void Main(string[] args)
+        {
+            Console.WriteLine("Content-type: text/html\r\n");
+            Console.WriteLine("<!DOCTYPE html>\r\n<html lang=\"en\">\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\r\n    <title>Document</title>\r\n</head>\r\n<body>\r\n    <form action=\"\" method=\"POST\">\r\n        <input type=\"text\" id=\"firstDigit\" name=\"firstDigit\">\r\n        <input type=\"text\" id=\"sign\" name=\"sign\">\r\n        <input type=\"text\" id=\"secondDigit\" name=\"secondDigit\">\r\n        <input type=\"submit\" value=\"Calculate\">\r\n    </form>\r\n</body>\r\n</html>");
+
+            var inputData = Console.ReadLine().Split('&');
+            try
+            {
+                var firstDigit = int.Parse(inputData[0].Split('=')[1]);
+                var secondDigit = int.Parse(inputData[2].Split('=')[1]);
+                var sign = WebUtility.UrlDecode(inputData[1].Split('=')[1]);
+
+                switch (sign)
+                {
+                    case "+":
+                        Console.WriteLine($"Result: {firstDigit + secondDigit}");
+                        break;
+                    case "-":
+                        Console.WriteLine($"Result: {firstDigit - secondDigit}");
+                        break;
+                    case "*":
+                        Console.WriteLine($"Result: {firstDigit * secondDigit}");
+                        break;
+                    case "/":
+                        Console.WriteLine($"Result: {firstDigit / secondDigit}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid sign!");break;
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Invalid Input");
+            }
+        }
+    }
+}
---
+++ C# Web Development Basics/Web Fundamentals Introduction + HTTP Protocol/Web Fundamentals Introduction HTTP/15.LoginForm/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _15.LoginForm
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Content-type: text/html\r\n");
+            Console.WriteLine("<!DOCTYPE html>\r\n<html lang=\"en\">\r\n\r\n<head>\r\n    <meta charset=\"UTF-8\">\r\n    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\r\n    <meta http-equiv=\"X-UA-Compatible\" content=\"ie=edge\">\r\n    <title>Document</title>\r\n</head>\r\n\r\n<body>\r\n    <form action=\"\" method=\"POST\">\r\n        <label for=\"username\">Username</label>\r\n        <input type=\"text\" id=\"username\" name=\"username\">\r\n        <br>\r\n        <lebel for=\"password\">Password</lebel>\r\n        <input type=\"password\" id=\"password\" name=\"password\">\r\n        <br>\r\n        <input type=\"submit\" value=\"Log in\">\r\n    </form>\r\n</body>\r\n\r\n</html>");
+
+            var data = Console.ReadLine().Split('&');
+            var name = data[0].Split('=')[1];
+            var password = data[1].Split('=')[1];
+
+            Console.WriteLine($"Hi {name}, your password is {password}");
+        }
+    }
+}
