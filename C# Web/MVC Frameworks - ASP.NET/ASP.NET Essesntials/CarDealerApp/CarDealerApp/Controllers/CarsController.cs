13bb214ef287d6547be2081b48a9cfd1fdf7101b
Vladimir Gadzhov <vladko_gadjov@abv.bg>
2017-05-01 17:24:25 +0300

    MVC Routes

---
+++ C# Web/MVC Frameworks - ASP.NET/ASP.NET Essesntials/CarDealerApp/CarDealerApp/Controllers/CarsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using CarDealer.Data;
+
+namespace CarDealerApp.Controllers
+{
+    public class CarsController : Controller
+    {
+        [HttpGet]
+        public ActionResult All(string make)
+        {
+            var ctx = new CarDealerContext();
+            var cars = ctx.Cars
+                .Where(c => c.Make == make)
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TravelledDistance)
+                .ToList();
+            if (cars.Count == 0)
+            {
+                throw new ArgumentException("Invalid input data!");
+            }
+
+            return View(cars);
+        }
+
+        [HttpGet]
+        public ActionResult About(int id)
+        {
+            var ctx = new CarDealerContext();
+            var car = ctx.Cars.Find(id);
+
+            return View(car);
+        }
+    }
+}
---
+++ C# Web/MVC Frameworks - ASP.NET/ASP.NET Essesntials/CarDealerApp/CarDealerApp/Controllers/CustomersController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using CarDealer.Data;
+using CarDealer.Models;
+
+namespace CarDealerApp.Controllers
+{
+    public class CustomersController : Controller
+    {
+        [HttpGet]
+        public ActionResult All(string order)
+        {
+            var ctx = new CarDealerContext();
+            IEnumerable<Customer> customers = null;
+
+            if (order == "ascending")
+            {
+                customers = ctx.Customers
+                    .OrderBy(c => c.BirthDate)
+                    .ThenBy(c => c.IsYoungDriver == false)
+                    .ToList();
+            }
+            else if (order == "descending")
+            {
+                customers = ctx.Customers
+                    .OrderByDescending(c => c.BirthDate)
+                    .ThenBy(c => c.IsYoungDriver == false)
+                    .ToList();
+            }
+
+            return View(customers);
+        }
+
+        [HttpGet]
+        public ActionResult About(int id)
+        {
+            var ctx = new CarDealerContext();
+            var custommer = ctx.Customers.Find(id);
+
+            return View(custommer);
+        }
+    }
+}
