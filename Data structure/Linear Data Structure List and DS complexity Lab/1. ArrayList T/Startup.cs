be2fc791ae7c5b4d3c81345c82b9da6d05758f38
Vladimir Gadzhov <vladimir.gadzhov@sirma.int>
2018-06-22 10:32:42 +0300

    Data structure

--- Data structure/Linear Data Structure List and DS complexity Lab/1. ArrayList T/Startup.cs
+++
@@ -1,7 +0,0 @@
-public class Startup
-{
-    public static void Main()
-    {
-
-    }
-}
--- Data structure/Linear-Data-Structures/Tests.ArrayList/Properties/AssemblyInfo.cs
+++ Data structure/Linear-Data-Structures/Tests.ArrayList/Properties/AssemblyInfo.cs
@@ -1,35 +1,35 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following
+// General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("1. ArrayList T")]
+[assembly: AssemblyTitle("Tests.ArrayList")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("1. ArrayList T")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyProduct("Tests.ArrayList")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible
-// to COM components.  If you need to access a type in this assembly from
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1229eff7-f102-410e-b54b-6d6a2dc95d16")]
+[assembly: Guid("7426af7e-9cdd-4c93-b4f3-f117a2273798")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version
+//      Minor Version 
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
+// You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
