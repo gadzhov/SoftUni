b4e60afdbd40e210a2131d47c1fd9b4ae16c1763
gadjov <vladko_gadjov@abv.bg>
2018-06-27 22:26:51 +0300

    Remove Odd Occurrences

---
+++ Data structure/Linear Data Structures - Exercise/4. Remove Odd Occurrences/Startup.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Remove_Odd_Occurrences
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var elements = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            foreach (var item in elements)
+            {
+                if (elements.Where(x => x == item).Count() % 2 == 0)
+                {
+                    Console.Write(item + " ");
+                }
+            }
+        }
+    }
+}
