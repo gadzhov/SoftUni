db4a083e903c5d509b18521c4a2ab0fc96c15326
gadjov <vladko_gadjov@abv.bg>
2018-06-27 22:37:28 +0300

    Count of Occurrences

---
+++ Data structure/Linear Data Structures - Exercise/5. Count of Occurrences/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.Count_of_Occurrences
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var collection = Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse);
+
+            var result = new Dictionary<int, int>();
+
+            foreach (var item in collection)
+            {
+                if (!result.ContainsKey(item))
+                {
+                    result.Add(item, 1);
+                }
+                else
+                {
+                    result[item]++;
+                }
+            }
+
+            foreach (var kv in result.OrderBy(k => k.Key))
+            {
+                Console.WriteLine($"{kv.Key} -> {kv.Value} times");
+            }
+        }
+    }
+}
