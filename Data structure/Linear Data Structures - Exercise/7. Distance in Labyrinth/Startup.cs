9a3e89116ab1acdb56d3f29233a6612601eba94a
gadjov <vladko_gadjov@abv.bg>
2018-06-28 19:33:28 +0300

    Distance in Labyrinth

--- Data structure/Linear Data Structures - Exercise/7. Distance in Labyrinth/Startup.cs
+++ Data structure/Linear Data Structures - Exercise/7. Distance in Labyrinth/Startup.cs
@@ -5,20 +5,13 @@ namespace _7.Distance_in_Labyrinth
 {
     public class Startup
     {
-        private static readonly string[,] Matrix =
-        {
-            {"0", "0", "0", "x", "0", "x"},
-            {"0", "x", "0", "x", "0", "x"},
-            {"0", "*", "x", "0", "x", "0"},
-            {"0", "x", "0", "0", "0", "0"},
-            {"0", "0", "0", "x", "x", "0"},
-            {"0", "0", "0", "x", "0", "x"}
-        };
+        private static string[,] Matrix = new string[0, 0];
 
         private static readonly Queue<Cell> VisitedCells = new Queue<Cell>();
 
-        private static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            PopulateMetrix();
             Cell startingCell = GetStartingPosition();
             VisitedCells.Enqueue(startingCell);
 
@@ -37,22 +30,35 @@ namespace _7.Distance_in_Labyrinth
 
             MarkUnreachable();
             PrintMatrix();
-
         }
 
-        static void PrintMatrix()
+        private static void PopulateMetrix()
+        {
+            var size = int.Parse(Console.ReadLine());
+            Matrix = new string[size, size];
+            for (int row = 0; row < size; row++)
+            {
+                var input = Console.ReadLine();
+                for (int col = 0; col < size; col++)
+                {
+                    Matrix[row, col] = input[col].ToString();
+                }
+            }
+        }
+
+        private static void PrintMatrix()
         {
             for (int i = 0; i < Matrix.GetLongLength(0); i++)
             {
                 for (int j = 0; j < Matrix.GetLongLength(1); j++)
                 {
-                    Console.Write("{0,4}", Matrix[i, j]);
+                    Console.Write(Matrix[i, j]);
                 }
                 Console.WriteLine();
             }
         }
 
-        static Cell GetStartingPosition()
+        private static Cell GetStartingPosition()
         {
             for (int i = 0; i < Matrix.GetLongLength(0); i++)
             {
