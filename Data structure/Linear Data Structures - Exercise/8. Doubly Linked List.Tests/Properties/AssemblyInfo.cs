359541dc2d26f3befde7090c29666a1c435e37a8
gadjov <vladko_gadjov@abv.bg>
2018-06-29 20:16:58 +0300

    Doubly Linked List

---
+++ Data structure/Linear Data Structures - Exercise/8. Doubly Linked List.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("8. Doubly Linked List.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("8. Doubly Linked List.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ad0771a2-adae-4537-972e-afd83a889ef6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Data structure/Linear Data Structures - Exercise/8. Doubly Linked List/DoublyLinkedList.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DoublyLinkedList<T> : IEnumerable<T>
+{
+    private ListNode<T> head;
+    private ListNode<T> tail;
+
+    public int Count { get; private set; }
+
+    public void AddFirst(T element)
+    {
+        if (this.Count == 0)
+        {
+            this.head = this.tail = new ListNode<T>(element);
+        }
+        else
+        {
+            var newHead = new ListNode<T>(element);
+            newHead.NextNode = this.head;
+            this.head.PrevNode = newHead;
+            this.head = newHead;
+        }
+
+        this.Count++;
+    }
+
+    public void AddLast(T element)
+    {
+        if (this.Count == 0)
+        {
+            this.tail = this.head = new ListNode<T>(element);
+        }
+        else
+        {
+            var newTail = new ListNode<T>(element);
+            newTail.PrevNode = this.tail;
+            this.tail.NextNode = newTail;
+            this.tail = newTail;
+        }
+
+        Count++;
+    }
+
+    public T RemoveFirst()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("Empty list.");
+        }
+
+        var firstElement = this.head.Vaue;
+        this.head = this.head.NextNode;
+
+        // There are more elements than 1.
+        if (this.head != null)
+        {
+            this.head.PrevNode = null;
+        }
+        else
+        {
+            this.tail = null;
+        }
+
+        this.Count--;
+
+        return firstElement;
+    }
+
+    public T RemoveLast()
+    {
+        if (this.Count == 0)
+        {
+            throw new InvalidOperationException("Empty list.");
+        }
+
+        var lastElement = this.tail.Vaue;
+        this.tail = tail.PrevNode;
+
+        // We more elements than 1.
+        if (this.tail != null)
+        {
+            this.tail.NextNode = null;
+        }
+        else
+        {
+            this.head = null;
+        }
+
+        this.Count--;
+
+        return lastElement;
+    }
+
+    public void ForEach(Action<T> action)
+    {
+        var currentNode = this.head;
+        while (currentNode != null)
+        {
+            action(currentNode.Vaue);
+            currentNode = currentNode.NextNode;
+        }
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        var current = this.head;
+        while (current != null)
+        {
+            yield return current.Vaue;
+            current = current.NextNode;
+        }
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return this.GetEnumerator();
+    }
+
+    public T[] ToArray()
+    {
+        var array = new T[this.Count];
+        var current = this.head;
+        var index = 0;
+        while (current != null)
+        {
+            array[index++] = current.Vaue;
+            current = current.NextNode;
+        }
+
+        return array;
+    }
+
+    #region helpers
+    private class ListNode<T>
+    {
+        public T Vaue { get; set; }
+
+        public ListNode<T> NextNode { get; set; }
+
+        public ListNode<T> PrevNode { get; set; }
+
+        public ListNode(T value)
+        {
+            this.Vaue = value;
+        }
+    }
+    #endregion
+}
