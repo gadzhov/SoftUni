954e03eae136972d16ef6097940b1ffe958cc966
gadjov <vladko_gadjov@abv.bg>
2018-06-30 23:09:38 +0300

    Stack and Queues - Exercises.

---
+++ Data structure/Stacks and Queues - Exercises/1. Reverse Numbers with a Stack/Startup.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace _1.Reverse_Numbers_with_a_Stack
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var result = new Stack(Console.ReadLine()
+                .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Console.WriteLine(string.Join(" ", result.ToArray()));
+        }
+    }
+}
