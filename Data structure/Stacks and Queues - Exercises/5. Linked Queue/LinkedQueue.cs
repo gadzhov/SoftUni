362bb9716f5f4fb7fffbfa01352dcf421b6c2a52
Vladimir Gadzhov <vladimir.gadzhov@sirma.bg>
2018-07-02 17:45:06 +0300

    Linked Queue skeleton.

---
+++ Data structure/Stacks and Queues - Exercises/5. Linked Queue/LinkedQueue.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class LinkedQueue<T>
+{
+    private QueueNode<T> head;
+    private QueueNode<T> tail;
+
+    public int Count { get; private set; }
+
+    public void Enqueue(T element)
+    {
+        if (this.Count == 0)
+        {
+            this.head = this.tail = new QueueNode<T>(element);
+        }
+
+        this.Count++;
+    }
+
+    public T Dequeue()
+    {
+        throw new NotImplementedException();
+    }
+
+    public T[] ToArray()
+    {
+        throw new NotImplementedException();
+    }
+
+    private class QueueNode<T>
+    {
+        public QueueNode(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; private set; }
+
+        public QueueNode<T> NextNode { get; private set; }
+
+        public QueueNode<T> PrevNode { get; private set; }
+    }
+}
